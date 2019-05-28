8b2c4bffb2730aaacd73c24498d5c0902ef4146f
Micael Mateus <mika.95@outlook.com>
2019-05-28 12:35:39 -0300

    Mensagem de boa noite

--- src/coursegit/CourseGit.java
+++ src/coursegit/CourseGit.java
@@ -9,6 +9,6 @@ public class CourseGit {
        
         System.out.println("Bom dia!");
         System.out.println("Boa tarde!");
-    }
-    
+        System.out.println("Boa noite!");
+    }    
 }
